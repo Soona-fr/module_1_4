ff118d24e249617851929cd202a300f3be741228
Soona-fr <il.ovechka@gmail.com>
2024-11-22 03:51:38 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input('Введите текст: ')
+print(len(my_string))
+#-----------------------------------------
+print(my_string .upper())
+print(my_string .lower())
+print(my_string .replace(' ',''))
+print('Первая буква вашего текста: ',my_string [0])
+print('Последняя буква вышего текста: ',my_string [-1])
